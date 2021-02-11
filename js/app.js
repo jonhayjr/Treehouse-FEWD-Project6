4a1c6652938e5f7637ac636dcdfa155aaa522d45
Jon Hay <jonhayjr@gmail.com>
2021-02-10 19:52:17 -0800

    Added Start Game button functionality

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const startButton = document.querySelector('.btn__reset');
+let missedGuesses = 0;
+const phrases = [
+'New York',
+'Santa Clause',
+'Professor',
+'Javascript',
+'Rocky Balboa'
+];
+
+//return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+//adds the letters of a string to the display   
+const addPhraseToDisplay = arr => {
+
+}
+
+//check if a letter is in the phrase
+const checkLetter = button => {
+
+
+
+}
+
+//check if the game has been won or lost
+const checkWin = () => {
+
+
+}
+
+//listen for the start game button to be pressed   
+startButton.addEventListener('click', () => {
+    const startScreen = document.querySelector('#overlay');
+    startScreen.style.display = 'none';
+});
+
+//list for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e=> {
+    
+});
